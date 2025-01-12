538ac76ab67aa1e29e66a128ab2b47bc3da117ac
Leon Welchert <lichturm@posteo.de>
2025-01-12 16:04:21 +0100

    add scripts for local development

--- localcontainer.sh
+++ localcontainer.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-hugo --minify
+set -euxo pipefail
+
+
+function cleanup() {
+  docker stop lichturm-website-debug-container
+}
+
+# nuke container when finished
+trap cleanup EXIT
+
+#hugo --minify
 
 docker build -t lichturm-website-local-build:debug .
+docker run --publish 8080:80 --detach --rm --name lichturm-website-debug-container lichturm-website-local-build:debug 
 
-docker run -p 8080:80 --rm lichturm-website-local-build:debug 
+firefox --private-window http://localhost:8080
+
+docker logs -f lichturm-website-debug-container
+
--- localdev.sh
+++ localdev.sh
@@ -1,1 +1,16 @@
-hugo serve --disableFastRender --ignoreCache --navigateToChanged --port 1313
+#!/bin/bash
+
+set -euxo pipefail
+
+
+# group commands and run in background
+{
+ echo "waiting 2s for hugo to start running..."
+ sleep 2
+ firefox --private-window http://localhost:1313
+} &
+
+hugo serve --disableFastRender --ignoreCache --navigateToChanged --port 1313 
+
+
+
