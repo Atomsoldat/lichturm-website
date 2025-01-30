eb167ec3b121ca2d4f8e12c1f30248ac8696722c
Leon Welchert <lichturm@posteo.de>
2025-01-30 01:21:22 +0100

    disable http cache during local testing

--- localdev.sh
+++ localdev.sh
@@ -10,7 +10,13 @@ set -euxo pipefail
  firefox --private-window http://localhost:1313
 } &
 
-hugo serve --buildDrafts=true --disableFastRender --ignoreCache --navigateToChanged --port 1313
+hugo serve \
+	--buildDrafts=true \
+	--disableFastRender \
+	--ignoreCache \
+	--noHTTPCache
+	--navigateToChanged \
+	--port 1313
 
 
 
